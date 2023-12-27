ba82d7726faf8ff55a483631bd760e51278076e3
Your Name <you@example.com>
2023-12-27 16:02:03 -0500

    goodcommit

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
           </code> */}
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
-          href="https://fprime.ai/solve"
+          href="https://docs.fprime.ai/solve"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -48,7 +48,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://fprime.ai/empower"
+          href="https://docs.fprime.ai/advanced"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -65,7 +65,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://fprime.ai/renew"
+          href="https://docs.fprime.ai/advanced"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
@@ -82,7 +82,7 @@ export default function Home() {
         </a>
 
         <a
-          href="https://fprime.ai/regenerate"
+          href="https://docs.fprime.ai/advanced"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
