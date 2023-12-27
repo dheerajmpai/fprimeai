eda7e9cff08d6dd63984e638f6e1b27ecd38f68e
Your Name <you@example.com>
2023-12-27 16:37:55 -0500

    goodcommit

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       </div> */}
       
       <code className={`font-mono font-bold mb-3 text-8xl font-semibold`}>
-            f`(AI){' '}
+            f'(ai){' '}
           </code>
         
       {/* <code className={`font-mono font-bold mb-3 text-2xl font-semibold`}>
